ed774e9044807000276d461efd4874ce7fcf0bc9
bayutnj-SIGAM-BOI <bayutnj@gmail.com>
2026-07-15 14:00:14 +0700

    Shooter constant

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants/ShooterConstant.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Constants;
+
+public final class ShooterConstant {
+    private ShooterConstant() {}
+
+
+    public static final double MAX_VELOCITY = 2300;
+    public static final double MAX_POWER = 1.0;
+    public static final double TICKS_TOLERANCE = 50; // Ticks
+}
